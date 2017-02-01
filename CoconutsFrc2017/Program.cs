69746f2cf3902a119c0fc763c80ca05adac2d948
choood <coyotewizard@gmail.com>
2017-02-01 13:43:11 -0800

    Project Setup
    -Removed unnecesary example files
    -Updated Nuget Packages

---
+++ CoconutsFrc2017/Program.cs
@@ -0,0 +1,18 @@
+﻿//DO NOT MODIFY THIS CLASS
+//This class automatically calls your robot class. Please place all user
+//code in the CoconutsFrc2017.cs class. Any changes made in here most likely
+//will cause the program to not run.
+
+using System.Reflection;
+using WPILib;
+
+namespace CoconutsFrc2017
+{
+    public class Program
+    {
+        static void Main()
+        {
+            RobotBase.Main(null, typeof(CoconutsFrc2017));
+        }
+    }
+}
